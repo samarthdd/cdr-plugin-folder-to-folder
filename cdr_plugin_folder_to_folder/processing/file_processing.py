280ad48744748f64c1cace37798724d8bafa3236
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-04-02 10:46:40 +0400

    Create file processing report

--- cdr_plugin_folder_to_folder/processing/file_processing.py
+++ cdr_plugin_folder_to_folder/processing/file_processing.py
@@ -1,8 +1,13 @@
 import sys
+import json
+import requests
+import ntpath
+import os.path
+
 sys.path.insert(1, '../common_settings')
 from config_params import Config
-import requests
-import json
+sys.path.insert(1, '../utils')
+from file_service import FileService
 
 class FileProcessing(object):
 
@@ -38,22 +43,61 @@ class FileProcessing(object):
     @staticmethod
     def filetypedetection (base64enc_file):
         return FileProcessing.base64request("api/FileTypeDetection/base64",base64enc_file)
+
+    @staticmethod
+    def create_report(hash, encodedFile):
+        xmlreport = FileProcessing.analyse(encodedFile)
+        if not xmlreport:
+            print("Cannot get xml report for {}", source_path)
+            return
+
+        report_folder = os.path.join(Config.hd2_location,"reports")
+        FileService.create_folder(report_folder)
+
+        report_file_folder = os.path.join(report_folder,hash)
+        FileService.create_folder(report_file_folder)
+
+        FileService.wrtie_file(report_file_folder,"report.xml",xmlreport)
         
+    @staticmethod
+    def processDirectory (dir):
+
+        hash = ntpath.basename(dir)
+        if len(hash) != 64:
+            print("Enexpected hash length for {}", dir)
+            return
+
+        source_path = os.path.join(dir, "source")
+        if not (FileService.file_exist(source_path)):
+            print("File does not exist {}", source_path)
+            return
+
+        encodedFile = FileService.base64encode(source_path)
+        if not encodedFile:
+            print("Cannot encode {}", source_path)
+            return
+
+        FileProcessing.create_report(hash, encodedFile)
 
     @staticmethod
     def main(argv):
         f = open("base64.sample", "r")
         base64_value = f.read()
 
-        result = FileProcessing.filetypedetection(base64_value)
-        print(result)
+        #result = FileProcessing.filetypedetection(base64_value)
+        #print(result)
 
-        result = FileProcessing.analyse(base64_value)
-        #report_path = os.path.join(Current.temp_folder, "report.xml")
-        print(result)
+        #result = FileProcessing.analyse(base64_value)
+        #print(result)
         
-        result = FileProcessing.rebuild(base64_value)
-        print(result)
+        #result = FileProcessing.rebuild(base64_value)
+        #print(result)
+
+        #file = open("sample.pdf", "wb")
+        #file.write(FileService.base64decode(result))
+        #file.close()
+
+        FileProcessing.processDirectory("C:\\gw_test\\hd2\\data\\32823a0dbe4dd137873cd286a592436ef738b10ce16e746a1ec64fb07c027615")
 
 if __name__ == "__main__":
     FileProcessing.main(sys.argv[1:])
