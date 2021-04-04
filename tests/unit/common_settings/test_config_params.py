3e6ca7328c8098fe3bc17a8111dc7277019364b5
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-04 11:56:57 +0100

    added fix to create test H2 and H3 folders

--- tests/unit/common_settings/test_config_params.py
+++ tests/unit/common_settings/test_config_params.py
@@ -1,5 +1,8 @@
 import os
 from unittest import TestCase
+
+from osbot_utils.utils.Files import folder_exists
+
 from cdr_plugin_folder_to_folder.common_settings.config_params import *
 
 class test_Config(TestCase):
@@ -8,9 +11,16 @@ class test_Config(TestCase):
         self.config  = Config().load_values()
 
     def test_load_values(self):
+        self.assertEqual(self.config.gw_sdk_address , os.environ.get("GW_SDK_ADDRESS" , DEFAULT_GW_SDK_ADDRESS))
+        self.assertEqual(self.config.gw_sdk_port    , int(os.environ.get("GW_SDK_PORT", DEFAULT_GW_SDK_PORT)))
         self.assertEqual(self.config.hd1_location   , os.environ.get("HD1_LOCATION"   , DEFAULT_HD1_LOCATION))
         self.assertEqual(self.config.hd2_location   , os.environ.get("HD2_LOCATION"   , DEFAULT_HD2_LOCATION))
         self.assertEqual(self.config.hd3_location   , os.environ.get("HD3_LOCATION"   , DEFAULT_HD3_LOCATION))
-        self.assertEqual(self.config.gw_sdk_address , os.environ.get("GW_SDK_ADDRESS" , DEFAULT_GW_SDK_ADDRESS))
-        self.assertEqual(self.config.gw_sdk_port    , int(os.environ.get("GW_SDK_PORT", DEFAULT_GW_SDK_PORT)))
+        self.assertEqual(self.config.root_folder    , os.environ.get("ROOT_FOLDER"    , DEFAULT_ROOT_FOLDER))
+
+
+        assert folder_exists(self.config.root_folder )
+        assert folder_exists(self.config.hd1_location)
+        assert folder_exists(self.config.hd2_location)
+        assert folder_exists(self.config.hd3_location)
 
