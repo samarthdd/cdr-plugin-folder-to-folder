603e1898002a02ac35d16e641d2c6afe62851d68
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-05 13:00:05 +0100

    added support for using docker-compose for Jupyter notebooks and mounted test_data volumes

--- bin/jupyter-docker-build-run.sh
+++ bin/jupyter-docker-build-run.sh
@@ -1,5 +1,8 @@
-cd notebooks
+cd jupyter
 docker build -t cdr_plugin_folder_to_folder_notebooks .
 
 #docker run --rm -it -p 8888:8888  cdr_plugin_folder_to_folder
-docker run --rm -it -p 8888:8888 -v $(PWD):/home/jovyan cdr_plugin_folder_to_folder_notebooks
+docker run --rm -it -p 8888:8888                        \
+        -v $(PWD)/notebooks:/home/jovyan/work           \
+        -v $(PWD)/../test_data:/home/jovyan/test_data   \
+        cdr_plugin_folder_to_folder_notebooks
