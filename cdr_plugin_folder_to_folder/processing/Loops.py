ab166906d30ed335208c43e3b81c76e87778644f
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-04-06 16:34:01 +0400

    Habdle failed Elastic connection case

--- cdr_plugin_folder_to_folder/processing/Loops.py
+++ cdr_plugin_folder_to_folder/processing/Loops.py
@@ -17,10 +17,18 @@ class Loops(object):
         rootdir = os.path.join(config.hd2_location,"data")
         directory_contents = os.listdir(rootdir)
         meta_service = Metadata_Service()
-        es = Elasticsearch([{'host': config.elastic_host, 'port': int(config.elastic_port)}])
 
-        # ignore 400 cause by IndexAlreadyExistsException when creating an index
-        es.indices.create(index='processed-index', ignore=400)
+        es = Elasticsearch()
+        use_es = False
+
+        try:
+            es = Elasticsearch([{'host': config.elastic_host, 'port': int(config.elastic_port)}])
+            # ignore 400 cause by IndexAlreadyExistsException when creating an index
+            es.indices.create(index='processed-index', ignore=400)
+            use_es = True
+        except Exception as error:
+            print("The connection to Elastic cannot be established")
+
         idx = 0
         for item in directory_contents:
             idx += 1
@@ -29,19 +37,21 @@ class Loops(object):
             if os.path.isdir(itempath):
                 try:
                     File_Processing.processDirectory(itempath)
-                    log = {
-                        'file': original_file_path,
-                        'status': 'processed',
-                        'error': 'none',
-                        'timestamp': datetime.now(),
-                    }
-                    es.index(index='processed-index', id=idx, body=log)
+                    if use_es:
+                        log = {
+                            'file': original_file_path,
+                            'status': 'processed',
+                            'error': 'none',
+                            'timestamp': datetime.now(),
+                        }
+                        es.index(index='processed-index', id=idx, body=log)
                 except Exception as error:
-                    log = {
-                        'file': original_file_path,
-                        'status': 'failed',
-                        'error': str(error),
-                        'timestamp': datetime.now(),
-                    }
-                    es.index(index='processed-index', id=idx, body=log)
+                    if use_es:
+                        log = {
+                            'file': original_file_path,
+                            'status': 'failed',
+                            'error': str(error),
+                            'timestamp': datetime.now(),
+                        }
+                        es.index(index='processed-index', id=idx, body=log)
                     print(error)
