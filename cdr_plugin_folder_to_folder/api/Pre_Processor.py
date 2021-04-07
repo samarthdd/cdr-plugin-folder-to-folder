6aea92da21e80f2c84d9816ca5348bbf6af8a2d8
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-07 14:20:43 +0100

    minor fix

--- cdr_plugin_folder_to_folder/api/Pre_Processor.py
+++ cdr_plugin_folder_to_folder/api/Pre_Processor.py
@@ -1,8 +1,8 @@
 from cdr_plugin_folder_to_folder.pre_processing.Pre_Processor import Pre_Processor
 from fastapi import APIRouter
 
-router_params = { "prefix": "/file-distributor"  ,
-                  "tags"  : ['File Distributor'] }
+router_params = { "prefix": "/pre-processor"  ,
+                  "tags"  : ['Pre Processor'] }
 router = APIRouter(**router_params)
 
 pre_processor = Pre_Processor()
