64b5cb4df5d4fe7ea7010bc05625ab0623171fe3
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-07 10:45:29 +0100

    added fix for "FastAPI reloading is not working"
    fixes #56

--- cdr_plugin_folder_to_folder/api/Server.py
+++ cdr_plugin_folder_to_folder/api/Server.py
@@ -1,3 +1,6 @@
+import logging
+
+import fastapi
 import uvicorn
 from fastapi import FastAPI
 
@@ -5,23 +8,35 @@ from fastapi import FastAPI
 from cdr_plugin_folder_to_folder.processing.main import router as processing_router
 from cdr_plugin_folder_to_folder.pre_processing.main import router as pre_processing_router
 
-
 class Server:
 
-    def __init__(self):
+    def __init__(self, app):
         self.host       = "0.0.0.0"
         self.log_level  = "info"
-        self.port       = 8880
-        self.app        = None
+        self.port       = 8880                                      # todo: move to globally configurable value (set via Env variables)
+        self.app        = app
+        self.reload     = True                                      # automatically reloads server on code changes
+
+    def fix_logging_bug(self):
+        # there were duplicated entries on logs
+        #    - https://github.com/encode/uvicorn/issues/614
+        #    - https://github.com/encode/uvicorn/issues/562
+        logging.getLogger().handlers.clear()                        # todo: see side effects of this
 
     def setup(self):
-        self.app = FastAPI()
         self.app.include_router(processing_router, prefix="")
         self.app.include_router(pre_processing_router, prefix="")
+        self.fix_logging_bug()
         return self
 
     def start(self):
-        uvicorn.run(self.app, host=self.host, port=int(self.port), log_level=self.log_level)
+        uvicorn.run("Server:app", host=self.host, port=int(self.port), log_level=self.log_level, reload=self.reload)
+
+
+# we need to do this here so that when unicorn reload is enabled the "Server:app" has an fully setup instance of the Server object
+app     = FastAPI()
+server  = Server(app)
+server.setup()
 
 if __name__ == "__main__":
-    Server().setup().start()
+    server.start()
--- tests/unit/api/test_Server.py
+++ tests/unit/api/test_Server.py
@@ -1,12 +1,18 @@
 from unittest import TestCase
 
+from fastapi import FastAPI
+
 from cdr_plugin_folder_to_folder.api.Server import Server
 
 
 class test_Server(TestCase):
 
     def setUp(self) -> None:
-        self.server = Server()
+        app = FastAPI()
+        self.server = Server(app=app)
 
     def test_setup(self):
         self.server.setup()
+
+    # def test_start(self):
+    #     self.server.setup().start()
