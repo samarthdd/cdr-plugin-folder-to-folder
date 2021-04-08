26a8b7c306d464407b82ed2a5ca95446fd57cb41
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2021-04-08 10:37:43 +0400

    Coding corrections

--- cdr_plugin_folder_to_folder/processing/File_Processing.py
+++ cdr_plugin_folder_to_folder/processing/File_Processing.py
@@ -12,13 +12,16 @@ from cdr_plugin_folder_to_folder.common_settings.Config import Config
 from cdr_plugin_folder_to_folder.utils.file_utils import FileService
 from cdr_plugin_folder_to_folder.metadata.Metadata_Service import Metadata_Service
 
-class File_Processing(object):                                       # todo: add Unit Tests to this class
+class File_Processing(object):
+
+    config = Config().load_values()
+    meta_service = Metadata_Service()
 
     @staticmethod
     def base64request(endpoint, base64enc_file):
-        config = Config().load_values()                             # todo refactor out of this method (since this should be loaded once, not everytime it is executed)
         try:
-            url = "http://" + config.gw_sdk_address + ":" + str(config.gw_sdk_port) + "/" + endpoint
+            url = "http://" + File_Processing.config.gw_sdk_address + ":" \
+                + str(File_Processing.config.gw_sdk_port) + "/" + endpoint
 
             payload = json.dumps({
               "Base64": base64enc_file
@@ -35,10 +38,10 @@ class File_Processing(object):                                       # todo: add
 
     @staticmethod
     def xmlreport_request(fileID):
-        config = Config().load_values()                             # todo refactor out of this method (since this should be loaded once, not everytime it is executed)
         try:
-            url = "http://" + config.gw_sdk_address + ":" + str(config.gw_sdk_port) \
-                  + "/api/Analyse/xmlreport?fileId=" + fileID
+            url = "http://" + File_Processing.config.gw_sdk_address \
+                + ":" + str(File_Processing.config.gw_sdk_port) \
+                + "/api/Analyse/xmlreport?fileId=" + fileID
 
             payload = ""
             headers = {
@@ -64,15 +67,6 @@ class File_Processing(object):                                       # todo: add
         return File_Processing.base64request("api/FileTypeDetection/base64", base64enc_file)
 
     @staticmethod
-    def create_report(hash, encodedFile, dir):
-        xmlreport = File_Processing.analyse(encodedFile).text
-        if not xmlreport:
-            raise ValueError('Failed to create the XML report')
-
-        json_obj = xmltodict.parse(xmlreport)
-        json_save_file_pretty(json_obj, os.path.join(dir, "report.json"))
-
-    @staticmethod
     def get_xmlreport(hash, fileId, dir):
         xmlreport = File_Processing.xmlreport_request(fileId)
         if not xmlreport:
@@ -88,8 +82,7 @@ class File_Processing(object):                                       # todo: add
         if not result:
             raise ValueError('Failed to rebuild the file')
 
-        meta_service = Metadata_Service()
-        processed_path = meta_service.get_processed_file_path(dir)
+        processed_path = File_Processing.meta_service.get_processed_file_path(dir)
 
         dirname = ntpath.dirname(processed_path)
         basename = ntpath.basename(processed_path)
@@ -117,11 +110,10 @@ class File_Processing(object):                                       # todo: add
         if len(hash) != 64:
             raise ValueError("Unexpected hash length")
 
-        meta_service = Metadata_Service()
-        if not meta_service.is_initial_status(dir):
+        if not File_Processing.meta_service.is_initial_status(dir):
             return
 
-        meta_service.set_status_inprogress(dir)
+        File_Processing.meta_service.set_status_inprogress(dir)
 
         source_path = os.path.join(dir, "source")
         if not (FileService.file_exist(source_path)):
@@ -138,7 +130,7 @@ class File_Processing(object):                                       # todo: add
 
         File_Processing.do_rebuild(hash, encodedFile, dir)
 
-        meta_service.set_status_comleted(dir)
+        File_Processing.meta_service.set_status_comleted(dir)
 
     @staticmethod
     def main(argv):
