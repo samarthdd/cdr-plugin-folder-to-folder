2d57e31e6c89f56c54a44ca516bbfb6b57bef7fc
samarthdd <samarth@smalldaytech.com>
2021-04-08 19:30:21 +0530

    refactor

--- cdr_plugin_folder_to_folder/api/routes/File_Distributor.py
+++ cdr_plugin_folder_to_folder/api/routes/File_Distributor.py
@@ -7,42 +7,35 @@ router        = APIRouter(**router_params)
 
 @router.get("/hd1/{num_of_files}")
 def get_hd1_files(num_of_files: int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd1_files(num_of_files=num_of_files)
+    list=File_Distributor().get_hd1_files(num_of_files=num_of_files)
     return list
 
 @router.get("/hd2/metadata/{num_of_files}")
 def get_hd2_metadata_files(num_of_files: int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd2_metadata_files(num_of_files=num_of_files)
+    list=File_Distributor().get_hd2_metadata_files(num_of_files=num_of_files)
     return list
 
 @router.get("/hd2/source/{num_of_files}")
 def get_hd2_source_files(num_of_files: int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd2_source_files(num_of_files=num_of_files)
+    list=File_Distributor().get_hd2_source_files(num_of_files=num_of_files)
     return list
 
 @router.get("/hd2/report/{num_of_files}")
 def get_hd2_report_files(num_of_files: int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd2_report_files(num_of_files=num_of_files)
+    list=File_Distributor().get_hd2_report_files(num_of_files=num_of_files)
     return list
 
 @router.get("/hd2/hash_folder_list/{num_of_files}")
 def get_hd2_hash_folders_files(num_of_files : int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd2_hash_folder_list(num_of_files=num_of_files)
+    list=File_Distributor().get_hd2_hash_folder_list(num_of_files=num_of_files)
     return list
 
 @router.get("/hd2/status")
 def get_hd2_status_files():
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd2_status_hash_file()
+    list=File_Distributor().get_hd2_status_hash_file()
     return list
 
 @router.get("/hd3/{num_of_files}")
 def get_hd3_files(num_of_files: int):
-    file_distributor = File_Distributor()
-    list=file_distributor.get_hd3_files(num_of_files=num_of_files)
+    list=File_Distributor().get_hd3_files(num_of_files=num_of_files)
     return list
