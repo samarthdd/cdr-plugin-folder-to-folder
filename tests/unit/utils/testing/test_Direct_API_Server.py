b8325ca4038eeebbe21f94a99e669b761170116e
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-08 02:05:43 +0100

    updating data

--- tests/unit/utils/testing/test_Direct_API_Server.py
+++ tests/unit/utils/testing/test_Direct_API_Server.py
@@ -6,14 +6,10 @@ from osbot_utils.utils.Dev import pprint
 from cdr_plugin_folder_to_folder.utils.testing.Direct_API_Server import Direct_API_Server
 
 
-#@pytest.mark.skip
 class test_Direct_API_Server(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.client = Direct_API_Server().setup()
-
-    #def setUp(self) -> None:
-    #    self.client = Direct_API_Server().setup()
 
     def test_GET(self):
         assert self.client.GET('/health') == {'status': 'ok'}
