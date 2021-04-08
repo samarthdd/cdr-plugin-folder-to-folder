65eea1ce8099e1c8c9c35c2cc85cb6cbc252a32b
Pranay <pranaysahith7@gmail.com>
2021-04-08 12:27:07 +0000

    update script to install using docker-compose

--- infra/packer/setup/setup.sh
+++ infra/packer/setup/setup.sh
@@ -9,24 +9,11 @@ fi
 # set hostname
 sudo hostnamectl set-hostname glasswall
 
-# install k3s
-curl -sfL https://get.k3s.io | sh -
-mkdir -p ~/.kube && sudo install -T /etc/rancher/k3s/k3s.yaml ~/.kube/config -m 600 -o $USER
-
-# install kubectl and helm
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-echo "Done installing kubectl"
-
-curl -sfL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-echo "Done installing helm"
-
 # get source code
 cd ~
 BRANCH=${BRANCH:-main}
 GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-filetrust/cdr-plugin-folder-to-folder}
-git clone https://github.com/${GITHUB_REPOSITORY}.git --branch $BRANCH && cd cdr-plugin-folder-to-folder
+git clone https://github.com/${GITHUB_REPOSITORY}.git --branch $BRANCH --recursive && cd cdr-plugin-folder-to-folder
 
 # build docker images
 sudo apt-get install \
@@ -42,32 +29,16 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # install local docker registry
 sudo docker run -d -p 30500:5000 --restart always --name registry registry:2
 
 # build images
 cd ~/cdr-plugin-folder-to-folder
-sudo docker build . -f cdr_plugin_folder_to_folder/Dockerfile -t localhost:30500/cdr-plugin-folder-to-folder
-sudo docker push localhost:30500/cdr-plugin-folder-to-folder
-sudo docker build jupyter -f jupyter/Dockerfile -t localhost:30500/cdr-plugin-f2f-jupyter
-sudo docker push localhost:30500/cdr-plugin-f2f-jupyter
-
-# install cdr plugin folder to folder API helm charts
-sudo mkdir -p /mnt/hd1 /mnt/hd2 /mnt/hd3
-GW_SDK_PORT=${GW_SDK_PORT:-1346}
-helm upgrade --install cdr-plugin-f2f \
-  --set image.cdrplugin.repository=localhost:30500/cdr-plugin-folder-to-folder \
-  --set image.cdrplugin.tag=latest \
-  --set image.jupyter.repository=localhost:30500/cdr-plugin-f2f-jupyter \
-  --set image.jupyter.tag=latest \
-  --set ingress.tls.enabled=false \
-  --set image.cdrplugin.env.HD1_LOCATION="/mnt/hd1" \
-  --set image.cdrplugin.env.HD2_LOCATION="/mnt/hd2" \
-  --set image.cdrplugin.env.HD3_LOCATION="/mnt/hd3" \
-  --set image.cdrplugin.env.GW_SDK_ADDRESS=$GW_SDK_ADDRESS \
-  --set image.cdrplugin.env.GW_SDK_PORT=$GW_SDK_PORT \
-  --atomic kubernetes/helm/chart/
+cp .env.sample .env
+echo "PWD=/home/ubuntu/cdr-plugin-folder-to-folder" >> .env
+sudo docker-compose up -d --build
 
 # create script to mount hard disks and upgrade helm chart
 tee -a > ~/setup.sh <<EOF
@@ -77,21 +48,9 @@ if [[ -z "$GW_SDK_ADDRESS" ]] ; then
     echo "Please pass glasswall SDK IP address as an argument"
     exit 0
 fi
-GW_SDK_PORT=${2-1346}
-mkdir -p /mnt/hd1 /mnt/hd2 /mnt/hd3
 
-helm upgrade --install cdr-plugin-f2f \
-  --set image.cdrplugin.repository=localhost:30500/cdr-plugin-folder-to-folder \
-  --set image.cdrplugin.tag=latest \
-  --set image.jupyter.repository=localhost:30500/cdr-plugin-f2f-jupyter \
-  --set image.jupyter.tag=latest \
-  --set ingress.tls.enabled=false \
-  --set image.cdrplugin.env.HD1_LOCATION="/mnt/hd1" \
-  --set image.cdrplugin.env.HD2_LOCATION="/mnt/hd2" \
-  --set image.cdrplugin.env.HD3_LOCATION="/mnt/hd3" \
-  --set image.cdrplugin.env.GW_SDK_ADDRESS=$GW_SDK_ADDRESS \
-  --set image.cdrplugin.env.GW_SDK_PORT=$GW_SDK_PORT \
-  --atomic kubernetes/helm/chart/
+cd ~/cdr-plugin-folder-to-folder
+sudo docker-compose up -d --build --force
 
 EOF
 chmod +x ~/setup.sh
