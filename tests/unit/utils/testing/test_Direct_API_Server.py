32365256ac9c4a0226c96d7e404eaf7d90487225
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-08 01:49:23 +0100

    another test

--- tests/unit/utils/testing/test_Direct_API_Server.py
+++ tests/unit/utils/testing/test_Direct_API_Server.py
@@ -8,12 +8,16 @@ from cdr_plugin_folder_to_folder.utils.testing.Direct_API_Server import Direct_A
 
 #@pytest.mark.skip
 class test_Direct_API_Server(TestCase):
-    def setUp(self) -> None:
-        self.client = Direct_API_Server().setup()
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.client = Direct_API_Server().setup()
+
+    #def setUp(self) -> None:
+    #    self.client = Direct_API_Server().setup()
 
     def test_GET(self):
         assert self.client.GET('/health') == {'status': 'ok'}
 
-    # def test__enter__(self):
-    #     with Direct_API_Server() as server:
-    #         assert server.GET('/health') == {'status': 'ok'}
+    def test__enter__(self):
+        with Direct_API_Server() as server:
+            assert server.GET('/health') == {'status': 'ok'}
