6899c08a769cea182a4166ba8189796e10dfc3a5
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-08 01:47:57 +0100

    testing GH actions hang scenario

--- tests/unit/utils/testing/test_Direct_API_Server.py
+++ tests/unit/utils/testing/test_Direct_API_Server.py
@@ -6,14 +6,14 @@ from osbot_utils.utils.Dev import pprint
 from cdr_plugin_folder_to_folder.utils.testing.Direct_API_Server import Direct_API_Server
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
 class test_Direct_API_Server(TestCase):
     def setUp(self) -> None:
-        self.api_server = Direct_API_Server().setup()
+        self.client = Direct_API_Server().setup()
 
     def test_GET(self):
-        assert self.api_server.GET('/health') == {'status': 'ok'}
+        assert self.client.GET('/health') == {'status': 'ok'}
 
-    def test__enter__(self):
-        with Direct_API_Server() as server:
-            assert server.GET('/health') == {'status': 'ok'}
+    # def test__enter__(self):
+    #     with Direct_API_Server() as server:
+    #         assert server.GET('/health') == {'status': 'ok'}
