d824835d19a86cb0351a0caf494c5b86b66d6f00
Pranay <pranaysahith7@gmail.com>
2021-04-08 13:00:09 +0000

    update permissions for docker-compose

--- infra/packer/setup/setup.sh
+++ infra/packer/setup/setup.sh
@@ -30,6 +30,7 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # install local docker registry
 sudo docker run -d -p 30500:5000 --restart always --name registry registry:2
