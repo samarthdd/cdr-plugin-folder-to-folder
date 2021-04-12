85d3dc29ca6830e9ff0672b96a01cc40c3fe3ce2
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-12 05:42:11 +0100

    adding elastic server to status method

--- cdr_plugin_folder_to_folder/api/Status.py
+++ cdr_plugin_folder_to_folder/api/Status.py
@@ -1,13 +1,19 @@
 from osbot_utils.utils.Misc import datetime_now
 
+from cdr_plugin_folder_to_folder.common_settings.Config import Config
+
 
 class Status:
 
     def __init__(self):
-        pass
+        self.config = Config().load_values()
 
     def now(self):
         data = {
+            "config": {
+                "elastic" : f"{self.config.elastic_schema}://{self.config.elastic_host}:{self.config.elastic_port}",
+                "kibana"  : f"{self.config.elastic_schema}://{self.config.kibana_host }:{self.config.kibana_port }"
+            },
             "date": datetime_now()
         }
 
