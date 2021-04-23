cf5960199449736c391928ceeb700c21637538ce
Dinis Cruz <dinis.cruz@owasp.org>
2021-04-23 01:45:30 +0100

    Added debug messages

--- tests/unit/pre_processing/test_Pre_Processor.py
+++ tests/unit/pre_processing/test_Pre_Processor.py
@@ -85,6 +85,11 @@ class test_Pre_Processor(TestCase):
 
         self.pre_processor.process_files()
 
+        pprint('**********: DATA')
+        pprint(files_list(path_data  ))
+        pprint('**********: STATUS')
+        pprint(files_list(path_status))
+        pprint('------------------------')
         assert len(files_list(path_data  )) > 0
         assert len(files_list(path_status)) == 2
 
