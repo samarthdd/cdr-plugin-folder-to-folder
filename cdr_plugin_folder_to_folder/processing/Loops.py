ee89a6257206d432fe8de4c5893cf2703a6330e2
Harut Grigoryan <harut70@hotmail.com>
2021-04-28 14:35:14 +0400

    Eliminating disk operations in Hash_Json

--- cdr_plugin_folder_to_folder/processing/Loops.py
+++ cdr_plugin_folder_to_folder/processing/Loops.py
@@ -87,9 +87,9 @@ class Loops(object):
                         'timestamp': datetime.now(),
                     }
                 log_info('ProcessDirectoryWithEndpoint', data=log_data)
-                #meta_service.set_error(itempath, "none")
-                #meta_service.set_status(itempath, FileStatus.COMPLETED)
-                #self.hash_json.update_status(file_hash, FileStatus.COMPLETED)
+                meta_service.set_error(itempath, "none")
+                meta_service.set_status(itempath, FileStatus.COMPLETED)
+                self.hash_json.update_status(file_hash, FileStatus.COMPLETED)
                 events.add_log("Has been processed")
                 return True
             except Exception as error:
