e34fa958bf8b3aa09114edb02feeb1d253eb6a79
samarthdd <samarth@smalldaytech.com>
2021-05-01 22:13:57 +0530

    fixed test cases

--- cdr_plugin_folder_to_folder/api/routes/File_Distributor.py
+++ cdr_plugin_folder_to_folder/api/routes/File_Distributor.py
@@ -18,7 +18,7 @@ def get_hd2_status_files():
     return FileResponse(file_response, media_type='application/octet-stream', filename=ntpath.basename(file_response))
 
 @router.get("/hd2/data")
-def get_hd2_processed_files(num_of_files: int = Query(-1, description="Keep -1 to get all hd2/data",)):
+def get_hd2_data_files(num_of_files: int = Query(-1, description="Keep -1 to get all hd2/data",)):
     if num_of_files == 0 :
         return "Invalid value for num_of_files"
     file_distributor = File_Distributor()
